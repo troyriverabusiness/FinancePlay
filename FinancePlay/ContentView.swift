ee6ab19e8a3e7cc170428bbc83fdb1f41e7d1c01
troy <troyriverabusiness@gmail.com>
2025-10-22 15:04:32 +0200

    Initial Commit

---
+++ FinancePlay/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FinancePlay
+//
+//  Created by troy on 22.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancePlay/FinancePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancePlayApp.swift
+//  FinancePlay
+//
+//  Created by troy on 22.10.25.
+//
+
+import SwiftUI
+
+@main
+struct FinancePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
