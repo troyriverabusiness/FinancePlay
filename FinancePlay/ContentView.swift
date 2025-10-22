7bbb972aa9471b23487814444d2e01666c52d722
troy <troyriverabusiness@gmail.com>
2025-10-22 15:22:08 +0200

    initial navigation

--- FinancePlay/ContentView.swift
+++ FinancePlay/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            
+            Tab {
+                Text("hello")
+            } label: {
+                Label("Markets", systemImage: "chart.line.uptrend.xyaxis")
+            }
+            Tab {
+                Text("world")
+            } label: {
+                Label("Portfolio", systemImage: "chart.pie")
+            }
+            Tab {
+                Text("Hello")
+            } label: {
+                Label("Education", systemImage: "books.vertical")
+            }
         }
     }
 }
